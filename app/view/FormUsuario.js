32d74d5e667c8ed6594cfe31d110e867526971f9
Lucian Fialho <lucianfialhobp@hotmail.com>
2013-08-15 09:43:54 -0300

    alinhando o layout do formulario de cadastro

--- app/view/FormUsuario.js
+++ app/view/FormUsuario.js
@@ -10,32 +10,58 @@ Ext.define('cf.view.FormUsuario', {
     border: 0,
 
     padding: 5,
-
-    items: [{
-        xtype: 'textfield',
-        name: 'nome',
-        fieldLabel: 'Nome'
-    },{
-        xtype: 'numberfield',
-        name: 'age',
-        fieldLabel: 'Idade'
-    },{
-        xtype: 'textfield',
-        name: 'phone',
-        fieldLabel: 'Telefone'
-    },{
-        xtype: 'radiofield',
-        name: 'alive',
-        inputValue: true,
-        fieldLabel: 'Vivo'
-    },{
-       xtype: 'radiofield',
-       name: 'alive' ,
-       inputValue: false,
-       fieldLabel: 'Morto'
-    },{
-        xtype: 'button',
-        text: 'Salvar',
-        name: 'salvar-formulario'
-    }]
+    defaults:{
+        width: 300,
+        margin: '5 0 0 10'  
+    },
+    items: [
+        {
+            xtype: 'textfield',
+            name: 'nome',
+            fieldLabel: 'Nome',
+        },
+        {
+            xtype: 'numberfield',
+            name: 'age',
+            fieldLabel: 'Idade',
+        },
+        {
+            xtype: 'textfield',
+            name: 'phone',
+            fieldLabel: 'Telefone',
+        },
+        {
+            xtype: 'fieldcontainer',
+            fieldLabel: 'Status',
+            defaultType: 'radiofield',
+            defaults: {
+                flex: 2
+            },
+            layout: 'hbox',
+            items: [
+                {   
+                    xtype: 'radiofield',
+                    name: 'alive',
+                    inputValue: true,
+                    fieldLabel: 'Vivo',
+                    width: 100,
+                    labelWidth: 50
+                },
+                {
+                    xtype: 'radiofield',
+                    name: 'alive' ,
+                    inputValue: false,
+                    fieldLabel: 'Morto',
+                    labelWidth: '50',
+                    width: 100,
+                    labelWidth: 50
+                }
+            ]
+        },
+        {
+            xtype: 'button',
+            text: 'Salvar',
+            name: 'salvar-formulario'
+        }
+    ]
 });
